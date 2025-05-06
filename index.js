d8d03b9300041591543cf91a0b4618a8edfccd82
StureDan <std001@edu.zealand.dk>
2025-05-06 13:59:53 +0200

    opdatering på html'en så den ser frisk og grøn ud. Desuden så fremviser den den seneste jordfugtighedsværdi

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
- const baseUrl = 'https://api.example.com/'; // Replace with your actual base URL
+const baseUrl = 'https://plantejuicingrest20250506131910.azurewebsites.net/api/Jordfugtighed'; // Replace with your actual base URL
+
 
  Vue.createApp({
     data() {
         return{
-            moisture: null
+            moisture: [],
+            moistureValue: 123
+            
         };
  },
 methods: {
     async getMoisture() {
         
             const response = await axios.get(baseUrl);
-            this.moisture = response.data.moisture;
+            this.moisture = response.data;
+            console.log(this.moisture);
+            if (this.moisture.length > 0) {
+                // Find objektet med det højeste id og gem kun jordfugtighedsværdien
+                const highestIdObject = this.moisture.reduce((max, item) =>
+                    item.id > max.id ? item : max
+                );
+                this.moistureValue = highestIdObject.jordfugtighedValue; // Antag at jordfugtighedsværdien er i feltet 'moistureValue'
+              }
+           
             
         
     }
